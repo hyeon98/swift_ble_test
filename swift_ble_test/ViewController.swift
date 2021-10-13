f42453b3e3a3addb3837b68b6d593f916a1bb5d2
hyeon98 <olreng97@gmail.com>
2021-10-13 22:35:41 +0900

    Initial Commit

---
+++ swift_ble_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_ble_test
+//
+//  Created by 정현석 on 2021/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ swift_ble_testTests/swift_ble_testTests.swift
@@ -0,0 +1,33 @@
+//
+//  swift_ble_testTests.swift
+//  swift_ble_testTests
+//
+//  Created by 정현석 on 2021/10/13.
+//
+
+import XCTest
+@testable import swift_ble_test
+
+class swift_ble_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
